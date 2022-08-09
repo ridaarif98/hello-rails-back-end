7f5ded42cb7554ebae73a7b1f875c3eb972eac34
ridaarif98 <ridaarif16@gmail.com>
2022-08-09 23:42:34 +0200

    Fix linter errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_211825) do
-
+ActiveRecord::Schema.define(version: 20_220_809_211_825) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "greetings", force: :cascade do |t|
-    t.string "message"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'greetings', force: :cascade do |t|
+    t.string 'message'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
--- test/models/greetig_test.rb
+++ test/models/greetig_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class GreetigTest < ActiveSupport::TestCase
   # test "the truth" do
